753a18aa040863da7914f0e7d2fec155c8c9bd48
omer <omertasci844@gmail.com>
2024-12-07 17:02:08 +0300

    db connect

--- data/db.js
+++ data/db.js
@@ -5,6 +5,8 @@ const config = require("config")
 
 
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password,{
+    host: config.db.host,
+    port:config.db.port,
     dialect: "mysql",
     storage: "./session.mysql"
 });
